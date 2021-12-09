9a57164470f602f861ed77c10fc96165fe4cf504
Zheng Jiang <momoxiaocaiji@gmail.com>
2021-12-09 15:07:26 -0500

    [+] Constant class

---
+++ src/bankUI/Constant.java
@@ -0,0 +1,7 @@
+package bankUI;
+
+public class Constant {
+    public static final int TRANSACTION_DEPOSIT = 1;
+    public static final int TRANSACTION_WITHDRAWAL = 2;
+    public static final int TRANSACTION_TRANSFER = 3;
+}
